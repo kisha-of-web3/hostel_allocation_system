c07bdc88f758cd5fe05a34df0be6961cfccf26d7
Kisha <Paschalbetiang@gmail.com>
2025-12-14 14:37:54 +0100

    debug settings

--- Hostel_assigning/settings.py
+++ Hostel_assigning/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-clh$zs!lok(_$(fwd2#nhn(-r838-f$-+yv=hp1h(rid(8v9*h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".onrender.com",
